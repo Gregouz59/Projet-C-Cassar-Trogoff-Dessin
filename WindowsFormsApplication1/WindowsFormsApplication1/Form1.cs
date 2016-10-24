ec12a8c69cad18aecf8ada3f99cc2bb0acc87fbd
Thomas CASSAR <thomas.cassar@isen-lille.fr>
2016-10-24 16:13:12 +0200

    Windows Form TEST

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -12,9 +12,55 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        int argent = 10;
+        int alcool = 0;
+
+
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Projet Bar";
+            textBox2.Text = "Argent: " + argent;
+            textBox3.Text = "Alcool: " + alcool;
+            textBox4.Text = "Heure";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bouton 2";
+            pictureBox1.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bouton 3";
+            pictureBox1.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bouton 4";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        
         }
     }
 }
